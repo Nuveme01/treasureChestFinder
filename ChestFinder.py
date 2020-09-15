6dc07b73bc311d0f9bec4331617df187c9f505cc
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-09-15 14:55:55 -0300

    Uploading the files

---
+++ ChestFinder.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+
+i = int(1)
+delay = int(1)
+
+while True:
+    chest = pyautogui.locateOnScreen('chest.png', confidence = 0.9)
+    if chest != None:
+        print('Clicked ' + str(i) + " time(s).")
+        i += 1
+        pyautogui.leftClick(chest)
+        pyautogui.moveTo(700, 400)
+    time.sleep(delay)
