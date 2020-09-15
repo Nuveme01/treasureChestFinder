6b2674c352747f70e330701894d60829831c81d2
Vinicius Fabio Amorim <Nuveme03@gmail.com>
2020-09-15 14:59:03 -0300

    Organizing

--- ChestFinder.py
+++ ChestFinder.py
@@ -5,7 +5,7 @@ i = int(1)
 delay = int(1)
 
 while True:
-    chest = pyautogui.locateOnScreen('chest.png', confidence = 0.9)
+    chest = pyautogui.locateOnScreen('Image\chest.png', confidence = 0.9)
     if chest != None:
         print('Clicked ' + str(i) + " time(s).")
         i += 1
